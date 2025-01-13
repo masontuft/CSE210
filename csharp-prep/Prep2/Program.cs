1fdbcccd52fcf483f7ff08ff8436c6c19bde9efb
masontuft <97546208+JMRCycling@users.noreply.github.com>
2025-01-13 10:55:32 -0700

    W01 Prove Assignment w/ stretch

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+        int grade = int.Parse(valueFromUser);
+        string gradeLetter;
+        string gradeSymbol = "";
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed your class!!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass! Try again, you got this!!");
+        }
+
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+            if ((grade % 10) <= 3)
+            {
+                gradeSymbol = "-";
+            }
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+            if ((grade % 10) >= 7)
+            {
+                gradeSymbol = "+";
+            }
+            else if ((grade % 10) <= 3)
+            {
+                gradeSymbol = "-";
+            }
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+            if ((grade % 10) >= 7)
+            {
+                gradeSymbol = "+";
+            }
+            else if ((grade % 10) <= 3)
+            {
+                gradeSymbol = "-";
+            }
+        }
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+            if ((grade % 10) >= 7)
+            {
+                gradeSymbol = "+";
+            }
+            else if ((grade % 10) <= 3)
+            {
+                gradeSymbol = "-";
+            }
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is a(n) {gradeLetter}{gradeSymbol} at {grade}%");
     }
 }

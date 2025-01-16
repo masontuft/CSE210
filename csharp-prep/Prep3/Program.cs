a2a311182a123b2b7483fb2bdba5853b5dfb7eb9
masontuft <97546208+JMRCycling@users.noreply.github.com>
2025-01-15 21:12:07 -0700

    Prep 3 Assignment Completion

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep 3 World!");
+        // Console.Write("What is the magic number? ");
+        // string magicInput = Console.ReadLine();
+        // int magicNumber = int.Parse(magicInput);
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        bool incorrect = true;
+        do 
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guessNumber = int.Parse(guess);
+            if (guessNumber == magicNumber) 
+            {
+                incorrect = false;
+                Console.WriteLine("You Guessed it!");
+            } else if (guessNumber >= magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }else 
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (incorrect);
+        
+
     }
 }

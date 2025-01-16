5f4f0dd815191b7fe4092d9484c4a92e86561125
masontuft <97546208+JMRCycling@users.noreply.github.com>
2025-01-15 22:07:45 -0700

    Prep 4 Completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep 4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = 1;
+
+        while(userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+            numbers.Add(userNumber);
+        }
+        // Console.WriteLine(numbers.Count);
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        // Console.WriteLine($"The average is: {numbers.Sum()/numbers.Count}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
     }
 }

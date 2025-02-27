5ee8ba6c693eed6fc463b9def26354c05a6b4ade
masontuft <97546208+JMRCycling@users.noreply.github.com>
2025-02-27 12:35:51 -0700

    Switch case added

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+        bool finished = false;
+        while (finished == false)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("\t1. Start breathing activity");
+            Console.WriteLine("\t2. Start reflecting activity");
+            Console.WriteLine("\t3. Start listing activity");
+            Console.WriteLine("\t4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            string userEntry = Console.ReadLine();
+            switch (userEntry)
+            {
+                case "1":
+                // Breathing activity
+                    break;
+                case "2":
+                // reflecting activity
+                    break;
+                case "3":
+                    // Listing Activity
+                    break;
+                case "4":
+                    finished = true;
+                    break;
+                default:
+                    break;
+            }
+
+        }
     }
 }

a073d62f19a4df01f9a5e4e57dff334431047076
masontuft <97546208+JMRCycling@users.noreply.github.com>
2025-03-06 13:10:24 -0700

    Base structure and test variables

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square(5, "blue");
+        Console.WriteLine($"Square area: {square.GetArea()}");
+        Console.WriteLine($"Square color: {square.GetColor()}");
+        square.SetColor("red");
+        Console.WriteLine($"Square color: {square.GetColor()}");
+        Circle circle = new Circle(5, "green");
+        Console.WriteLine($"Circle area: {circle.GetArea()}");
+        Console.WriteLine($"Circle color: {circle.GetColor()}");
+        circle.SetColor("yellow");
+        Console.WriteLine($"Circle color: {circle.GetColor()}");
+        Rectangle rectangle = new Rectangle(5, 10, "purple");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}");
+        rectangle.SetColor("orange");
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}");
+        
     }
 }
